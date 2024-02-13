bd7fb405b8fca7a75222ed66c577f80b81a3f5fd
Bryce Wren <wrenba@g.cofc.edu>
2024-02-13 18:49:17 -0500

    set-up react front end and back end

---
+++ TrashApp/client/src/Routes.js
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
+import { HomePage } from './pages/HomePage';
+import { Events } from './pages/Events';
+
+
+
+export const AppRoutes = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path='/' element={<Login />} />
+                <Route path='/register' element={<Register />} />
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/events" element={<Events />} />
+            </Routes>
+        </Router>
+    )
+}
