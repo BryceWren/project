8a580becf697b4493dc3ec3d47f63d233b529012
NfaithK <kelleyf@g.cofc.edu>
2024-02-18 16:48:11 -0500

    IDK how to run it

--- TrashApp/client/src/Components/LoginC.jsx
+++ TrashApp/client/src/Components/LoginC.jsx
@@ -1,5 +1,6 @@
 // this is  the  entire login page
 import React from 'react'
+import './CSS/login.CSS'
 
 const LoginC = () => {
   return (
@@ -11,20 +12,20 @@ const LoginC = () => {
     {/*  This is where the UserName goes  */}
     <div classname ="type boxes"> 
     <div classname=" Username">
-    <input type=" Text" />
+    <input type=" Text" placeholder="JohnDoe93" />
     </div>
     {/*  this is where the password  goes  */}
     <div classname ="type boxes"> 
     <div classname="Password">
-    <input type=" Text" />
+    <input type=" Password" placeHolder= "**********" />
     </div>
     <div classname= "The container"> 
     <div classname ="Submit button">
       <p>Login</p>
     </div>
      </div>
-    <div classname="newUser?"><p>Are you A new User <a href="">Click Here!</a></p></div> {/* whoever  does the registration page this is where the link goes :) */}
-
+    <div classname="newUser?"><p classname="new user?">Are you A new User <a href="">Click Here!</a></p></div> {/* whoever  does the registration page this is where the link goes :) also may turn this into a button, i just  need opinions */}
+ 
     </div>
     </div>
     </div>
