66fae24442b4fd0f194a7ef6438a673993a604ba
NfaithK <kelleyf@g.cofc.edu>
2024-02-21 17:25:28 -0500

    part of the css for login

--- TrashApp/client/src/Components/LoginC.jsx
+++ TrashApp/client/src/Components/LoginC.jsx
@@ -1,38 +1,39 @@
 // this is  the  entire login page
-import React from 'react'
-import './CSS/Login.css'
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+import './CSS/Login.css';
 
 export const LoginC = () => {
   return (
-    <div classname="Container"><h1></h1>
-    <div classname="header">
-    <div classname="Title"><h1>Sign In</h1></div>
-    <div classname ="underline"></div>
+    <div className="Container">
+    <div className="header">
+    <div className="Title"><h1>Sign In</h1></div>
+    <div className ="underline"></div>
     </div>
     {/*  This is where the UserName goes  */}
-    <div classname ="type boxes"> 
-    <div classname=" Username">
+    <div className ="typeBoxes"> 
+    <div className=" Username">
     <input type=" Text" placeholder="JohnDoe93" />
     </div>
     {/*  this is where the password  goes  */}
-    <div classname ="type boxes"> 
-    <div classname="Password">
+    <div className ="typeBoxes"> 
+    <div className="Password">
     <input type=" Password" placeHolder= "**********" />
     </div>
-    <div classname= "The container"> 
-    <div classname ="Submit button">
-    <button classname="Link-btn" onClick={() => navigate ('/home')}>Login</button>     
+    <div className= "The container"> 
+    <div className ="Submit button">
+    <button className="Link-btn" onClick={() => useNavigate ('/home')}>Login</button>     
     {/* this is for the  login button and where ever its suppose to route too :) */}
     </div>
      </div>
-    <div classname="newUser?">
-    <p>Dont Have an account? </p><button classname="Link-btn" onClick={() => navigate ('/register')}> Register Here</button>     
+    <div className="newUser?">
+    <p>Dont Have an account? </p><button classname="Link-btn" onClick={() => UseNavigate ('/')}> Register Here</button>     
     </div> {/* whoever  does the registration page this is where the link goes :) also may turn this into a button, i just  need opinions */}
  
- 
     </div>
     </div>
     </div>
+    
 
 
   
