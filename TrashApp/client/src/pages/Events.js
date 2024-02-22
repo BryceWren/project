15bd30be7d12452696e3d852c1b3b379ec7bc1a0
Bryce Wren <wrenba@g.cofc.edu>
2024-02-22 15:38:31 -0500

    got routes set up for the settings navigation bar

---
+++ TrashApp/client/src/pages/Events.js
@@ -0,0 +1,12 @@
+import React from "react"
+import HomePage from "./HomePage"
+
+
+export const Events = ()=> {
+    return (
+<div>
+    <p>"events page"</p>
+</div>
+    )
+}
+export default HomePage
--- TrashApp/client/src/pages/Settings.js
+++ TrashApp/client/src/pages/Settings.js
@@ -5,11 +5,27 @@ import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import CalendarMonthRoundedIcon from '@mui/icons-material/CalendarMonthRounded';
 import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
-import { useNavigate } from 'react-router-dom';
+import { Route, useNavigate } from 'react-router-dom';
 
 
 export const Settings = () => {
   const [value, setValue] = React.useState(0);
+  const navigate = useNavigate();
+
+  const handleHomeClick = () => {
+    setValue(0);
+    navigate('/home');
+  };
+
+  const handleEventsClick = () => {
+    setValue(1);
+    navigate('/events');
+  };
+
+  const handleSettingsClick = () => {
+    setValue(2);
+    navigate('/settings');
+  };
 
   return (
     <Box sx={{ width: 500 }}>
@@ -20,12 +36,16 @@ export const Settings = () => {
           setValue(newValue);
         }}
       >
-        <BottomNavigationAction label="Home" icon={<HomeRoundedIcon />} />
-        <BottomNavigationAction label="Events" icon={<CalendarMonthRoundedIcon />} />
-        <BottomNavigationAction label="Settings" icon={<SettingsRoundedIcon />} />
+        
+        
+        <BottomNavigationAction label="Home" icon={<HomeRoundedIcon />} onClick={handleHomeClick} />
+        <BottomNavigationAction label="Events" icon={<CalendarMonthRoundedIcon />} onClick={handleEventsClick}/>
+        <BottomNavigationAction label="Settings" icon={<SettingsRoundedIcon />} onClick={handleSettingsClick}/>
       </BottomNavigation>
     </Box>
   );
 }
 
 export default Settings;
+
+//<button className="logoutbtn" onClick={() => navigate('/')}>LOGOUT</button>
