4be5c35bf59de4d93d9c47b789f8977730aec161
janica <jaramore@gmail.com>
2024-02-23 21:15:39 -0500

    nav on events page

--- TrashApp/client/src/pages/Events.js
+++ TrashApp/client/src/pages/Events.js
@@ -1,11 +1,16 @@
 import React from "react"
 import HomePage from "./HomePage"
+import NavigationBar from '../Components/NavBar';
 
 
 export const Events = ()=> {
     return (
 <div>
     <p>"events page"</p>
+    <div>
+      <NavigationBar />
+    </div> 
+    {/* Add events body here */}
 </div>
     )
 }
